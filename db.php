ba5231036627c64ed4d969321c16d3b6902cf39c
domenicobacoli <domenico.bacoli89@gmail.com>
2023-05-04 17:45:38 +0200

    feat: added content on page with simple style

--- db.php
+++ db.php
@@ -5,10 +5,10 @@ require_once './Models/FoodProduct.php';
 require_once './Models/GameProduct.php';
 require_once './Models/KennelProduct.php';
 
-$frisbee = new GameProduct("Frisbee", 4, "For Fan Pets", "https://www.moby-dick.it/image/cache/data/Foto%20Prodotti/Cani/giochi/for-fan-pets-frisbee-avengers-capitan-america-licenza-ufficiale-marvel-600x600.jpg", 10, "123456", "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt", "cane"); 
-$mechanicalMouse = new GameProduct("Topo Meccanico", 6, "For Fan Pets", "https://www.moby-dick.it/image/cache/data/Foto%20Prodotti/Cani/giochi/for-fan-pets-frisbee-avengers-capitan-america-licenza-ufficiale-marvel-600x600.jpg", 8, "123456", "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt", "gatto");
-$croquettes = new FoodProduct("Crocchette per cane", 5, "Miglior Cane", "https://www.moby-dick.it/image/cache/data/Foto%20Prodotti/Cani/giochi/for-fan-pets-frisbee-avengers-capitan-america-licenza-ufficiale-marvel-600x600.jpg", 6, "123456", "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt", "cane", "28-06-2023");
-$tidbits = new FoodProduct("Crocchette per cane", 5, "Miglior Gatto", "https://www.moby-dick.it/image/cache/data/Foto%20Prodotti/Cani/giochi/for-fan-pets-frisbee-avengers-capitan-america-licenza-ufficiale-marvel-600x600.jpg", 8, "123456", "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt", "gatto", "28-06-2023");
+$frisbee = new GameProduct("Frisbee Avengers", 4, "For Fan Pets", "https://www.moby-dick.it/image/cache/data/Foto%20Prodotti/Cani/giochi/for-fan-pets-frisbee-avengers-capitan-america-licenza-ufficiale-marvel-600x600.jpg", 10, "123456", "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt", "Cane"); 
+$mechanicalMouse = new GameProduct("Topo Meccanico", 6, "For Fan Pets", "https://www.moby-dick.it/image/cache/data/Foto%20Prodotti/Gatti/giochi/camon-gioco-topo-meccanico-600x600.jpg", 8, "123456", "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt", "Gatto");
+$croquettes = new FoodProduct("Crocchette per cane", 12, "Miglior Cane", "https://media.franzysonline.it/media/catalog/product/cache/0b83d9404a26713075055d01f5be6584/m/i/miglior-cane-crocchette-wellness-system-manzo-64560-2.jpg", 6, "123456", "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt", "Cane", "28-06-2023");
+$tidbits = new FoodProduct("Crocchette per gatto", 5, "Miglior Gatto", "https://www.isoladeitesori.it/dw/image/v2/BGRZ_PRD/on/demandware.static/-/Sites-it-master-catalog/default/dw58962b8c/idt/503150_2.jpg?sw=1000&sh=1000", 8, "123456", "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt", "Gatto", "28-06-2023");
 
 
 $products = [
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+require_once './db.php';
+?>
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -12,11 +16,33 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <!-- importo l'header con la navbar  -->
+    <div class="app-container">
+            <!-- importo l'header con la navbar  -->
     <?php include './Views/partials/header.php'; ?>
+        <div class="card-container">
+            <?php foreach($products as $product) { ?>
+            <div class="card">
+                <div class="card-image">
+                    <img src="<?php echo $product->image; ?>" alt="Frisbee">
+                </div>
+                <div class="card-details">
+                    <div><strong>NOME:</strong><?php echo $product->name; ?></div>
+                    <div><strong>PREZZO:</strong><?php echo $product->price; ?>€</div>
+                    <div><strong>MARCA:</strong><?php echo $product->brand; ?></div>
+                    <div><strong>DISPONIBILITA':</strong><?php echo $product->availability; ?></div>
+                    <div><strong>CODICE PRODOTTO:</strong><?php echo $product->productCode; ?></div>
+                    <div><strong>CATEGORIA:</strong><?php echo $product->category; ?></div>
+                    <div><strong>DESCRIZIONE:</strong><?php echo $product->description; ?></div>
+                </div>    
+            </div>
+            <?php
+            }
+            ?>
+        </div>
 
     <!-- importo il footer -->
     <?php include './Views/partials/footer.php'; ?>
+    </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
