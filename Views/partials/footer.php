e036c0a5e7e4621ed830af399f36c6a39b961315
domenicobacoli <domenico.bacoli89@gmail.com>
2023-05-04 14:32:45 +0200

    first commit

---
+++ Views/partials/footer.php
@@ -0,0 +1,8 @@
+<footer class="bg-light text-center text-lg-start mt-5">
+  <!-- Copyright -->
+  <div class="text-center p-3">
+    © 2023 Copyright:
+    <a class="text-dark" href="#">DomenicoBacoli.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
